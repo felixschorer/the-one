6714458cda265ae356a7f899754833c6e34d8e31
Simon Zachau <simon.zachau@tum.de>
2019-11-24 20:40:54 +0100

    generate random schedule

--- src/movement/NodeGridMovement.java
+++ src/movement/NodeGridMovement.java
@@ -2,7 +2,6 @@ package movement;
 
 import core.Coord;
 import core.Settings;
-import core.SimClock;
 import movement.map.MapNode;
 import movement.map.SimMap;
 import movement.nodegrid.NodeGridSettings;
@@ -11,10 +10,15 @@ import movement.pathfinding.AStarPathFinder;
 import movement.pathfinding.Heuristic;
 import movement.pathfinding.PathFinder;
 import movement.pathfinding.RandomizedDistanceHeuristic;
+import movement.scheduling.Event;
+import movement.scheduling.Schedule;
 
 import java.util.*;
 
 public class NodeGridMovement extends MovementModel implements RenderableMovement {
+    private static final int LECTURE_HALL = 1;
+    private static final int COLLECTION_AREA = 2;
+
     private static OSM2NodeGrid osm2NodeGridCache = null;
 
     private Set<MapNode> pointsOfInterest;
@@ -28,6 +32,8 @@ public class NodeGridMovement extends MovementModel implements RenderableMovemen
     private Event nextEvent;
 
     private Schedule schedule;
+
+    private Map<Integer, List<Event>> eventsByTimeslot;
 
     public NodeGridMovement(Settings settings) {
         super(settings);
@@ -43,6 +49,26 @@ public class NodeGridMovement extends MovementModel implements RenderableMovemen
 
         Heuristic heuristic = new RandomizedDistanceHeuristic(rng::nextGaussian, 2);
         pathFinder = new AStarPathFinder(heuristic);
+
+        // generate all events
+        eventsByTimeslot = new HashMap<>();
+        int offset = 1000;
+        int timeStart = offset;
+        // 1 day, 5 2-hour slots between 8am and 6pm,
+        // each room is occupied all the time,
+        // start cum tempore, end cum tempore
+        for (int i = 0; i < 5; i++) {
+            int ct = 60 * 15;
+            timeStart += ct;
+            int timeEnd = timeStart + 60 * 90;
+            eventsByTimeslot.put(timeStart, new ArrayList<>());
+            for (MapNode location : pointsOfInterest) {
+                if (location.isType(LECTURE_HALL)) {
+                    eventsByTimeslot.get(timeStart).add(new Event(location, timeStart, timeEnd));
+                }
+            }
+            timeStart += timeEnd + ct;
+        }
     }
 
     public NodeGridMovement(NodeGridMovement other) {
@@ -50,7 +76,7 @@ public class NodeGridMovement extends MovementModel implements RenderableMovemen
         nodeGrid = other.nodeGrid;
         pointsOfInterest = other.pointsOfInterest;
         pathFinder = other.pathFinder;
-        schedule = new Schedule(pointsOfInterest);
+        schedule = new Schedule(other.eventsByTimeslot, rng);
     }
 
     @Override
@@ -86,7 +112,7 @@ public class NodeGridMovement extends MovementModel implements RenderableMovemen
     @Override
     public double nextPathAvailable() {
         double estimatedTime = estimateTravelTime(currentNode, nextEvent.getLocation(), 1);
-        return nextEvent.getTimestamp() - estimatedTime;
+        return nextEvent.getTimestampStart() - estimatedTime;
     }
 
     @Override
