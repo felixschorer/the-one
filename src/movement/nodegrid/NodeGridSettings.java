3ce18bf7ea6fb6abc11ebc1ef7f3c8ff0e277500
Felix Schorer <felixschorer@users.noreply.github.com>
2019-11-26 14:42:10 +0100

    Add missing equals method to NodeGridSettings

--- src/movement/nodegrid/NodeGridSettings.java
+++ src/movement/nodegrid/NodeGridSettings.java
@@ -2,6 +2,8 @@ package movement.nodegrid;
 
 import core.Settings;
 
+import java.util.Objects;
+
 public class NodeGridSettings {
     private static final String NODE_GRID_NS = "NodeGrid";
     private static final String RASTER_INTERVAL = "rasterInterval";
@@ -22,5 +24,19 @@ public class NodeGridSettings {
 
     public String getOsmFile() {
         return osmFile;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NodeGridSettings that = (NodeGridSettings) o;
+        return Double.compare(that.getRasterInterval(), getRasterInterval()) == 0 &&
+                getOsmFile().equals(that.getOsmFile());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRasterInterval(), getOsmFile());
     }
 }
