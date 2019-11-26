60af879df8c78a49ebb7027cfccb0dfe0ee11dc2
Simon Zachau <simon.zachau@tum.de>
2019-11-26 12:20:41 +0100

    different durations of lectures

--- src/movement/fmi/Lecture.java
+++ src/movement/fmi/Lecture.java
@@ -1,22 +1,27 @@
 package movement.fmi;
 
+import core.Coord;
 import movement.map.MapNode;
 
 import java.util.Random;
 
 public class Lecture extends Event {
-    static public int CT_INCREMENT_START[] = { 0, 60 * 15, 60 * 30 };
-    static public int TOTAL_DURATION = 60 * 120;
+    static public int CT_INCREMENT_START_OPTIONS[] = { 0, 60 * 15, 60 * 30 };
+    static public int TOTAL_DURATION_OPTIONS[] = { 60 * 120, 60 * 120 * 2 };
     static public int TOTAL_CT = 60 * 30;
 
+    private int totalDuration; // including ct
     private int ctIncrementStart;
     private int ctIncrementEnd;
 
     public Lecture(MapNode location, int timestampStart, Random rng) {
         super(location, timestampStart);
 
-        int randomCtIndex = rng.nextInt(CT_INCREMENT_START.length);
-        ctIncrementStart = CT_INCREMENT_START[randomCtIndex];
+        int randomCtIndex = rng.nextInt(CT_INCREMENT_START_OPTIONS.length);
+        ctIncrementStart = CT_INCREMENT_START_OPTIONS[randomCtIndex];
+
+        int randomDurationIndex = rng.nextInt(TOTAL_DURATION_OPTIONS.length);
+        totalDuration = TOTAL_DURATION_OPTIONS[randomDurationIndex];
         ctIncrementEnd = TOTAL_CT - ctIncrementStart;
     }
 
@@ -28,7 +33,13 @@ public class Lecture extends Event {
         return ctIncrementStart;
     }
 
+    public int getTotalDuration() { return totalDuration; }
+
     public int getCtIncrementEnd() {
         return ctIncrementEnd;
+    }
+
+    public int getRealEnd() {
+        return getTimestampStart() + getTotalDuration() - getCtIncrementEnd();
     }
 }
--- src/movement/fmi/Schedule.java
+++ src/movement/fmi/Schedule.java
@@ -4,28 +4,48 @@ import movement.map.MapNode;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Schedule {
     private LinkedList<Event> schedule;
+    private int SWITCHING_PLACE_TIME = 60 * 15;
 
-    public Schedule(Map<Integer, List<Event>> eventsByTimeslot, ArrayList<MapNode> collectionAreas, Random rng) {
-        // pick random events
-        schedule = eventsByTimeslot.keySet().stream()
-                .sorted()
-                .map(eventsByTimeslot::get)
-                .map(events -> {
-                    int locationIndex = rng.nextInt(events.size());
-                    return events.get(locationIndex);
-                })
-                .map(event -> {
-                    if (rng.nextBoolean()) {
-                        return event;
-                    }
+    public Schedule(ArrayList<Lecture> lectures, ArrayList<MapNode> collectionAreas, Random rng) {
+        lectures.sort(Comparator.comparing(Lecture::getRealStart));
 
-                    int collectionAreaIndex = rng.nextInt(collectionAreas.size());
-                    return new Event(collectionAreas.get(collectionAreaIndex), event.getTimestampStart());
-                })
+        LinkedList<Event> lectureSchedule = new LinkedList<>();
+        LinkedList<Event> otherSchedule = new LinkedList<>();
+
+        // fill up time with lectures
+        int currentScheduleTime = 0;
+        for (Lecture lecture : lectures) {
+            int currentAvailableTime = currentScheduleTime + SWITCHING_PLACE_TIME;
+            boolean isLectureTimeDoable = currentAvailableTime <= lecture.getRealStart();
+            boolean isOtherEventPreferred = rng.nextBoolean();
+            if (isLectureTimeDoable && !isOtherEventPreferred) {
+                lectureSchedule.add(lecture);
+                currentScheduleTime += lecture.getRealEnd();
+            }
+        }
+
+        // go somewhere else between lectures if time allows
+        for (int i = 1; i < lectureSchedule.size(); i++) {
+            Lecture lecture1 = (Lecture) lectureSchedule.get(i - 1);
+            Lecture lecture2 = (Lecture) lectureSchedule.get(i);
+            int currentAvailableTime = lecture1.getRealEnd() + SWITCHING_PLACE_TIME;
+
+            if (lecture2.getRealStart() - currentAvailableTime > 0) {
+                int collectionAreaIndex = rng.nextInt(collectionAreas.size());
+                Event otherEvent = new Event(collectionAreas.get(collectionAreaIndex), currentAvailableTime);
+                otherSchedule.add(otherEvent);
+            }
+        }
+
+        schedule = Stream.of(lectureSchedule, otherSchedule)
+                .flatMap(x -> x.stream())
                 .collect(Collectors.toCollection(LinkedList::new));
+
+        schedule.sort(Comparator.comparing(Event::getRealStart));
     }
 
     public boolean hasNextEvent() {
