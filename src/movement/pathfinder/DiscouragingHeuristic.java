937770d6b732e8e1569685108f41fd34515cfa58
Felix Schorer <felixschorer@users.noreply.github.com>
2019-11-29 20:50:32 +0100

    Add comment

--- src/movement/pathfinder/DiscouragingHeuristic.java
+++ src/movement/pathfinder/DiscouragingHeuristic.java
@@ -14,6 +14,7 @@ public class DiscouragingHeuristic implements Heuristic {
     @Override
     public double compute(MapNode from, MapNode to) {
         if (from != to && from.isType(notOkMapNodes)) {
+            // not a valid a start heuristic, however, but that's exactly what we want here
             return 10000 + heuristic.compute(from, to);
         }
         return heuristic.compute(from, to);
