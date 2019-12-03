277cb0d7256afbc60ad92d3ae663971d7ec59692
Felix Schorer <felixschorer@users.noreply.github.com>
2019-12-03 22:00:14 +0100

    Add node density report for all 4 layers

---
+++ src/report/university/NodeDensityReportLayer3.java
@@ -0,0 +1,10 @@
+package report.university;
+
+import report.NodeDensityReport;
+
+public class NodeDensityReportLayer3 extends NodeDensityReport {
+    public NodeDensityReportLayer3() {
+        super();
+        layer = 3;
+    }
+}
